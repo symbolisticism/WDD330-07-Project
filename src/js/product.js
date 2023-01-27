dd07c7fe39dd11682059a2f9f4590edd475d9e0f
ogbaududavid <ogbaudu.dc@gmail.com>
2023-01-27 19:52:56 +0100

    week 4 team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
