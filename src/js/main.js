3fb607eb902a6054b4d6ad8d27128c9a0e134f3f
chimamodesty <chimavic2000@gmail.com>
2023-02-02 22:33:43 +0100

    completed team work for week 5

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,11 @@
 import ProductData from "./ProductData.mjs";
-<<<<<<< HEAD
-import ProductListing from "./ProductList.mjs";
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductListing("tents", dataSource, element);
-
-console.log(listing);
-listing.init()
-=======
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+loadHeaderFooter();
+
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
 
 listing.init();
->>>>>>> dd07c7fe39dd11682059a2f9f4590edd475d9e0f
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -39,6 +39,42 @@ export function renderListWithTemplate(
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
 
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if(callback) {
+      callback(data);
+  }
+}
+
+async function loadTemplate(path){
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
+
+
+
+{
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -47,3 +83,9 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+
+
+
+
+
