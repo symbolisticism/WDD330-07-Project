12ef0d5b37aa7edd7f5d3bfe15d40f8a2fa4d33e
ogbaududavid <ogbaudu.dc@gmail.com>
2023-02-10 22:36:45 +0100

    team5 activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,3 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-<<<<<<< HEAD
-import ProductListing from "./ProductList.mjs";
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductListing("tents", dataSource, element);
-
-console.log(listing);
-listing.init()
-=======
-import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
-
-<<<<<<< HEAD
-listing.init();
->>>>>>> dd07c7fe39dd11682059a2f9f4590edd475d9e0f
-=======
-listing.init();
->>>>>>> 7088e1a35ba14f3c72a519169e8e4653cae57800
